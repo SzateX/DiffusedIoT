9ce0626e25f33278287b5a50e96c5cdc31cf7988
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-10-17 01:19:46 +0200

    Initial Commit

---
+++ MQTTApi/enums.py
@@ -0,0 +1,27 @@
+class DeviceType:
+    GENERIC_TEMPERATURE_SENSOR = 'GTS'
+    GENERIC_HUMIDITY_SENSOR = 'GHS'
+    GENERIC_LAMP = 'GL'
+
+
+class UnitDirection:
+    INPUT = 'input'
+    OUTPUT = 'output'
+    IN_OUT = 'inout'
+
+
+class UnitType:
+    SWITCH_UNIT = 'switch'
+    TEMPERATURE_UNIT = 'temperature'
+    HUMIDITY_UNIT = 'humidity'
+
+
+class TemperatureUnit:
+    CELSIUS = 'celsius'
+    KELVIN = 'kelvin'
+    FAHRENHEIT = 'fahrenheit'
+
+
+class HumidityUnit:
+    PERCENTAGE = 'percentage'
+    GRAMS_PER_METER_SQUARED = 'grams per meter squared'
