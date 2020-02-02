31ad103e685fa702878cdabb61e01128df5c6842
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2020-02-02 22:04:36 +0100

    Corrected dir path

--- MQTTHub/config.py
+++ MQTTHub/config.py
@@ -2,7 +2,7 @@
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
     'default': {
